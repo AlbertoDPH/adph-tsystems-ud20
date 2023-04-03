34c9ca2fe1adea93f9298426f8f6a46c323ccf94
AlbertoDPH <114424982+AlbertoDPH@users.noreply.github.com>
2023-04-03 11:32:27 +0200

    UD20_01
    Ejercicio1 Primer commit

---
+++ ud20_01/src/main/java/com/delpozo/ud20_01/App.java
@@ -0,0 +1,13 @@
+package com.delpozo.ud20_01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
